38ff5da1d8836726974320de2b62fcc3d91cade2
bkawk <email.will.in.china@gmail.com>
2019-07-18 19:10:34 +0800

    manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './images/agenix.png';
+import logo from './images/agenix.svg';
 import './App.scss';
 
 const App: React.FC = () => {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="Agenix Logo" />
